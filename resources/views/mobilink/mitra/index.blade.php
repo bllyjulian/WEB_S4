6264725ce0c69c6d1a4209d6ca7ecc9213e0a6f3
bllyjulian <billyjulian75@gmail.com>
2024-06-08 15:54:58 +0700

    UPDATE : Nambah Lokasi Toko di tabel Mitra

--- resources/views/mobilink/mitra/index.blade.php
+++ resources/views/mobilink/mitra/index.blade.php
@@ -7,6 +7,7 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/owlcarousel.css')}}">
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/rating.css')}}">
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/sweetalert2.css')}}">
+<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
 <style>
     .table th:nth-child(7) {
         width: 40px;
@@ -36,37 +37,48 @@
                     <div class="table-responsive product-table">
                         <table class="display" id="basic-1">
                             <thead>
-                            <tr>
-                                <th>Logo Toko</th>
-                                <th>Nama Toko</th>
-                                <th>Username</th>
-                                <th>Nama Pemilik</th>
-                                <th>Nomor Hp</th>
-                                <th>Tanggal Daftar</th>
-                                <th>Status</th>
-                            </tr>
+                                <tr>
+                                    <th>Logo Toko</th>
+                                    <th>Username</th>
+                                    <th>Nama Pemilik</th>
+                                    <th>Nomor Hp</th>
+                                    <th>Tanggal Daftar</th>
+                                    <th>Lokasi</th>
+                                    <th>Status</th>
+                                </tr>
                             </thead>
                             <tbody>
                             @foreach ($mitra as $item)
-                            <tr>
-                                <td><img style="width: 50px; border-radius: 50%;" src="{{ asset($item->logo_mitra) }}" alt=""></td>
-                                <td>{{ $item->nama_toko }}</td>
-                                <td>{{ $item->username }}</td>
-                                <td>{{ $item->nama_lengkap }}</td>
-                                <td>{{ $item->nomor_hp }}</td>
-                                <td>{{ $item->tgl_daftar }}</td>
-                                <td>
-                                @if ($item->status_akun == 1)
-    <button style="border-radius:5px;" type="button" class="btn btn-success" onclick="editAkun('{{ $item->username }}')">Aktif</button>
-@elseif ($item->status_akun == 2)
-    <button style="border-radius:5px;" type="button" class="btn btn-danger" onclick="aktifAkun('{{ $item->username }}')">Dibanned</button>
-@elseif ($item->status_akun == 3)
-    <button style="border-radius:5px;" type="button" class="btn btn-dark" onclick="aktifAkun('{{ $item->username }}')">Ditolak</button>
-@else
-    <button style="border-radius:5px;" type="button" class="btn btn-info" onclick="accAkun('{{ $item->username }}')">Pending</button>
-@endif
-                                </td>
-                            </tr>
+                                <tr>
+                                    <td style="display: flex; flex-direction: column; align-items: center;">
+                                        <img style="width: 50px; border-radius: 50%;" src="{{ asset($item['logo_mitra']) }}" alt="">
+                                        <span style="text-align: center;" class="order-process-circle" style="margin-top: 5px;">{{ $item['nama_toko'] }}</span>
+                                    </td>
+                                    <td>{{ $item->username }}</td>
+                                    <td>{{ $item->nama_lengkap }}</td>
+                                    <td>{{ $item->nomor_hp }}</td>
+                                    <td>{{ $item->tgl_daftar }}</td>
+                                    <td>
+                                        @if($item['latitude'] != null)
+                                            <a class="tombolmaps" href="#" data-username="{{ $item['username'] }}" data-toggle="modal" data-target="#modalLokasi{{ $item['username'] }}">
+                                                Lihat Lokasi
+                                            </a>
+                                        @else
+                                            Belum ada
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if ($item->status_akun == 1)
+                                            <button style="border-radius:5px;" type="button" class="btn btn-success" onclick="editAkun('{{ $item->username }}')">Aktif</button>
+                                        @elseif ($item->status_akun == 2)
+                                            <button style="border-radius:5px;" type="button" class="btn btn-danger" onclick="aktifAkun('{{ $item->username }}')">Dibanned</button>
+                                        @elseif ($item->status_akun == 3)
+                                            <button style="border-radius:5px;" type="button" class="btn btn-dark" onclick="aktifAkun('{{ $item->username }}')">Ditolak</button>
+                                        @else
+                                            <button style="border-radius:5px;" type="button" class="btn btn-info" onclick="accAkun('{{ $item->username }}')">Pending</button>
+                                        @endif
+                                    </td>
+                                </tr>
                             @endforeach
                             </tbody>
                         </table>
@@ -76,6 +88,30 @@
         </div>
     </div>
 </div>
+
+@foreach($mitra as $item)
+<div class="modal fade" id="modalLokasi{{ $item['username'] }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Lokasi Toko</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <!-- Tambahkan div untuk menampilkan peta -->
+                <div id="mapContainer{{ $item['username'] }}" style="height: 400px;"></div>
+                <input type="hidden" id="latitude{{ $item['username'] }}" value="{{ $item['latitude'] }}">
+                <input type="hidden" id="longitude{{ $item['username'] }}" value="{{ $item['longitude'] }}">
+                <input type="hidden" id="namaToko{{ $item['username'] }}" value="{{ $item['nama_toko'] }}">
+
+            </div>
+        </div>
+    </div>
+</div>
+@endforeach
+
 @endsection
 
 @section('script')
@@ -85,9 +121,48 @@
 <script src="{{ asset('assets/js/rating/rating-script.js')}}"></script>
 <script src="{{ asset('assets/js/ecommerce.js')}}"></script>
 <script src="{{ asset('assets/js/product-list-custom.js')}}"></script>
+<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script>
+$('.tombolmaps').click(function() {
+    var username = $(this).data('username');
+    setTimeout(function() {
+        initializeMap(username);
+    }, 500); // Ubah timeout menjadi 500 milidetik atau lebih
+});
+
+function initializeMap(username) {
+    $('#mapContainer' + username).css('height', '400px'); // Sesuaikan tinggi peta
+    var latitude = parseFloat($('#latitude' + username).val()); // Ambil latitude dari input tersembunyi
+    var longitude = parseFloat($('#longitude' + username).val()); // Ambil longitude dari input tersembunyi
+    var namaToko = $('#namaToko' + username).val(); // Ambil nama toko dari input tersembunyi
+
+    var map = L.map('mapContainer' + username, {
+        minZoom: 5,
+        attributionControl: false // Menyembunyikan teks atribusi Leaflet
+    }).setView([latitude, longitude], 13);
+
+    // Tambahkan layer peta
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
+
+    // Tambahkan marker
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup(namaToko) // Gunakan nama toko sebagai teks pada marker
+        .openPopup();
+
+    // Resize peta saat modal ditampilkan
+    $('#modalLokasi' + username).on('shown.bs.modal', function() {
+        setTimeout(function() {
+            map.invalidateSize();
+        }, 500);
+    });
+}
+
+
 function editAkun(username) {
     Swal.fire({
         title: 'Apakah Anda yakin ingin membanned akun ini?',
--- storage/framework/views/3bea7761c52af4eb04ca65bc7e6d65bf.php
+++ storage/framework/views/3bea7761c52af4eb04ca65bc7e6d65bf.php
@@ -5,6 +5,7 @@
 <link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/owlcarousel.css')); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/rating.css')); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo e(asset('assets/css/sweetalert2.css')); ?>">
+<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
 <style>
     .table th:nth-child(7) {
         width: 40px;
@@ -34,37 +35,48 @@
                     <div class="table-responsive product-table">
                         <table class="display" id="basic-1">
                             <thead>
-                            <tr>
-                                <th>Logo Toko</th>
-                                <th>Nama Toko</th>
-                                <th>Username</th>
-                                <th>Nama Pemilik</th>
-                                <th>Nomor Hp</th>
-                                <th>Tanggal Daftar</th>
-                                <th>Status</th>
-                            </tr>
+                                <tr>
+                                    <th>Logo Toko</th>
+                                    <th>Username</th>
+                                    <th>Nama Pemilik</th>
+                                    <th>Nomor Hp</th>
+                                    <th>Tanggal Daftar</th>
+                                    <th>Lokasi</th>
+                                    <th>Status</th>
+                                </tr>
                             </thead>
                             <tbody>
                             <?php $__currentLoopData = $mitra; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                            <tr>
-                                <td><img style="width: 50px; border-radius: 50%;" src="<?php echo e(asset($item->logo_mitra)); ?>" alt=""></td>
-                                <td><?php echo e($item->nama_toko); ?></td>
-                                <td><?php echo e($item->username); ?></td>
-                                <td><?php echo e($item->nama_lengkap); ?></td>
-                                <td><?php echo e($item->nomor_hp); ?></td>
-                                <td><?php echo e($item->tgl_daftar); ?></td>
-                                <td>
-                                <?php if($item->status_akun == 1): ?>
-    <button style="border-radius:5px;" type="button" class="btn btn-success" onclick="editAkun('<?php echo e($item->username); ?>')">Aktif</button>
-<?php elseif($item->status_akun == 2): ?>
-    <button style="border-radius:5px;" type="button" class="btn btn-danger" onclick="aktifAkun('<?php echo e($item->username); ?>')">Dibanned</button>
-<?php elseif($item->status_akun == 3): ?>
-    <button style="border-radius:5px;" type="button" class="btn btn-dark" onclick="aktifAkun('<?php echo e($item->username); ?>')">Ditolak</button>
-<?php else: ?>
-    <button style="border-radius:5px;" type="button" class="btn btn-info" onclick="accAkun('<?php echo e($item->username); ?>')">Pending</button>
-<?php endif; ?>
-                                </td>
-                            </tr>
+                                <tr>
+                                    <td style="display: flex; flex-direction: column; align-items: center;">
+                                        <img style="width: 50px; border-radius: 50%;" src="<?php echo e(asset($item['logo_mitra'])); ?>" alt="">
+                                        <span style="text-align: center;" class="order-process-circle" style="margin-top: 5px;"><?php echo e($item['nama_toko']); ?></span>
+                                    </td>
+                                    <td><?php echo e($item->username); ?></td>
+                                    <td><?php echo e($item->nama_lengkap); ?></td>
+                                    <td><?php echo e($item->nomor_hp); ?></td>
+                                    <td><?php echo e($item->tgl_daftar); ?></td>
+                                    <td>
+                                        <?php if($item['latitude'] != null): ?>
+                                            <a class="tombolmaps" href="#" data-username="<?php echo e($item['username']); ?>" data-toggle="modal" data-target="#modalLokasi<?php echo e($item['username']); ?>">
+                                                Lihat Lokasi
+                                            </a>
+                                        <?php else: ?>
+                                            Belum ada
+                                        <?php endif; ?>
+                                    </td>
+                                    <td>
+                                        <?php if($item->status_akun == 1): ?>
+                                            <button style="border-radius:5px;" type="button" class="btn btn-success" onclick="editAkun('<?php echo e($item->username); ?>')">Aktif</button>
+                                        <?php elseif($item->status_akun == 2): ?>
+                                            <button style="border-radius:5px;" type="button" class="btn btn-danger" onclick="aktifAkun('<?php echo e($item->username); ?>')">Dibanned</button>
+                                        <?php elseif($item->status_akun == 3): ?>
+                                            <button style="border-radius:5px;" type="button" class="btn btn-dark" onclick="aktifAkun('<?php echo e($item->username); ?>')">Ditolak</button>
+                                        <?php else: ?>
+                                            <button style="border-radius:5px;" type="button" class="btn btn-info" onclick="accAkun('<?php echo e($item->username); ?>')">Pending</button>
+                                        <?php endif; ?>
+                                    </td>
+                                </tr>
                             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                             </tbody>
                         </table>
@@ -74,6 +86,30 @@
         </div>
     </div>
 </div>
+
+<?php $__currentLoopData = $mitra; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+<div class="modal fade" id="modalLokasi<?php echo e($item['username']); ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Lokasi Toko</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <!-- Tambahkan div untuk menampilkan peta -->
+                <div id="mapContainer<?php echo e($item['username']); ?>" style="height: 400px;"></div>
+                <input type="hidden" id="latitude<?php echo e($item['username']); ?>" value="<?php echo e($item['latitude']); ?>">
+                <input type="hidden" id="longitude<?php echo e($item['username']); ?>" value="<?php echo e($item['longitude']); ?>">
+                <input type="hidden" id="namaToko<?php echo e($item['username']); ?>" value="<?php echo e($item['nama_toko']); ?>">
+
+            </div>
+        </div>
+    </div>
+</div>
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('script'); ?>
@@ -83,9 +119,48 @@
 <script src="<?php echo e(asset('assets/js/rating/rating-script.js')); ?>"></script>
 <script src="<?php echo e(asset('assets/js/ecommerce.js')); ?>"></script>
 <script src="<?php echo e(asset('assets/js/product-list-custom.js')); ?>"></script>
+<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script>
+$('.tombolmaps').click(function() {
+    var username = $(this).data('username');
+    setTimeout(function() {
+        initializeMap(username);
+    }, 500); // Ubah timeout menjadi 500 milidetik atau lebih
+});
+
+function initializeMap(username) {
+    $('#mapContainer' + username).css('height', '400px'); // Sesuaikan tinggi peta
+    var latitude = parseFloat($('#latitude' + username).val()); // Ambil latitude dari input tersembunyi
+    var longitude = parseFloat($('#longitude' + username).val()); // Ambil longitude dari input tersembunyi
+    var namaToko = $('#namaToko' + username).val(); // Ambil nama toko dari input tersembunyi
+
+    var map = L.map('mapContainer' + username, {
+        minZoom: 5,
+        attributionControl: false // Menyembunyikan teks atribusi Leaflet
+    }).setView([latitude, longitude], 13);
+
+    // Tambahkan layer peta
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
+
+    // Tambahkan marker
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup(namaToko) // Gunakan nama toko sebagai teks pada marker
+        .openPopup();
+
+    // Resize peta saat modal ditampilkan
+    $('#modalLokasi' + username).on('shown.bs.modal', function() {
+        setTimeout(function() {
+            map.invalidateSize();
+        }, 500);
+    });
+}
+
+
 function editAkun(username) {
     Swal.fire({
         title: 'Apakah Anda yakin ingin membanned akun ini?',
@@ -232,4 +307,5 @@ function accAkun(username) {
 
 </script>
 <?php $__env->stopSection(); ?>
+
 <?php echo $__env->make('layouts.simple.master_mobilink', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\webS4\resources\views/mobilink/mitra/index.blade.php ENDPATH**/ ?>
